5a30879725fe5d69248a6342ad0efbff9767a3c2
LorenzoBee <danbailey.813@gmail.com>
2021-08-28 00:16:31 +0100

    Added consistent colours

--- src/components/ClosestHoliday.vue
+++ src/components/ClosestHoliday.vue
@@ -2,17 +2,32 @@
     <div class="closest-holiday-container">
         <h1>Closest Holiday</h1>
         <div class="closest-holiday">
-            <v-sheet tile height="54" class="d-flex">
-                <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
-                    <v-icon>mdi-chevron-left</v-icon>
-                </v-btn>
-                <v-btn icon class="ma-2" @click="$refs.calendar.next()">
-                    <v-icon>mdi-chevron-right</v-icon>
-                </v-btn>
-            </v-sheet>
-            <v-sheet>
-                <v-calendar :events="events" ref="calendar" :start="Date.parse(days[0].start)" min-weeks="8"/>
-            </v-sheet>
+          <div>{{days[0].start}} - {{days[0].end}}</div>
+          <v-sheet tile class="d-flex">
+              <v-btn 
+              icon
+              class="ma-2"
+              @click="$refs.calendar.prev()">
+                  <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn 
+              icon
+              class="ma-2"
+              @click="$refs.calendar.next()">
+                  <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+              <v-toolbar-title v-if="$refs.calendar">
+                {{ $refs.calendar.title }}
+              </v-toolbar-title>
+          </v-sheet>
+          <v-sheet>
+              <v-calendar
+              v-model="start"
+              :events="events"
+              ref="calendar"
+              min-weeks="7"
+              :show-month-on-first="false"/>
+          </v-sheet>
             <!-- <span>{{ days.duration }} days holiday with {{ days.noOfDays }} days booked!</span>
             <ul>
                 <li v-for="(item, i) in days.allDates" :key="i" :class="days.daysOff.includes(item) ? 'book' : days.holidays.includes(days.allDates[i]) ? 'public-holiday' : 'no-book'">
@@ -32,14 +47,18 @@ export default {
   },
   data: () => ({
     events: [],
+    colours: ['orange', 'green', 'cyan', 'red'],
+    start: ""
   }),
   mounted() {
-    
+    this.start=this.days[0].date;
     for (let day of this.days) {
       let newEvent = {};
+
       day.allDates.forEach((event, index) => {
         if (newEvent.name != event.title) { //new event
           if (newEvent.name != null) {
+            newEvent.color = this.getColour(newEvent.name);
             this.events.push(newEvent);
             newEvent = {};
           }
@@ -53,17 +72,26 @@ export default {
         }
         else if (index == day.allDates.length - 1){
           newEvent.end = event.date;
+          newEvent.color = this.getColour(newEvent.name);
           this.events.push(newEvent);
         }
       });
           this.events.push({
-              name: "Total",
+              name: "Total: " + day.allDates.length + " days",
               start: day.allDates[0].date, 
               end: day.allDates[day.allDates.length - 1].date,
-              color: "red"
+              color: this.colours[3]
               });
     }
   },
+  methods: {
+    rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+    },
+    getColour(title) {
+      return title.includes("Book") ? this.colours[0] : title.includes("Weekend") ? this.colours[1] : this.colours[2];
+    }
+  }
 };
 </script>
 
@@ -80,52 +108,9 @@ body {
 
 .closest-holiday-container {
   text-align: center;
+}
 
-  ul {
-    li {
-      display: inline-block;
-      padding: 1rem 1.5rem 3rem 1.5rem;
-      border: 1px solid black;
-      position: relative;
-      color: black;
-
-      &:after {
-        position: absolute;
-        left: 0;
-        bottom: 0;
-        background: $decoration-colour;
-        color: $decoration-text-colour;
-        width: 100%;
-        height: auto;
-        padding: 0.2rem 0;
-      }
-    }
-
-    .book {
-      span {
-        background: $book-colour;
-      }
-
-      &:after {
-        content: "Book";
-      }
-    }
-
-    .no-book {
-      span {
-        background: $default-color;
-      }
-    }
-
-    .public-holiday {
-      span {
-        background: $default-color;
-      }
-
-      &:after {
-        content: "Public Holiday";
-      }
-    }
-  }
+.theme--light.v-calendar-weekly .v-calendar-weekly__day.v-outside{
+  background: #ececec !important;
 }
 </style>
